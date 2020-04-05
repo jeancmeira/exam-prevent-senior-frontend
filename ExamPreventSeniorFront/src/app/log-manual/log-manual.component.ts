04ad89cd5d23fdb6a6d6d608cc26a679769e685c
JeanDev <Jean@192.168.0.10>
2020-04-05 18:31:16 -0300

    Commit - Commit de funcionalidade

---
+++ ExamPreventSeniorFront/src/app/log-manual/log-manual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-manual',
+  templateUrl: './log-manual.component.html',
+  styleUrls: ['./log-manual.component.css']
+})
+export class LogManualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
